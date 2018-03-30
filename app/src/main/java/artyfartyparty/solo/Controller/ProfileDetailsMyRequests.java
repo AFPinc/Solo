c4adfc13212bdca9b6bfbb7ff26f38abbe2afacb
asajulia <aja11@hi.is>
2018-03-30 15:48:21 +0000

    allar skrár tengt profileinu

---
+++ app/src/main/java/artyfartyparty/solo/Controller/ProfileDetailsMyRequests.java
@@ -0,0 +1,13 @@
+package artyfartyparty.solo.Controller;
+
+import android.os.Bundle;
+
+/**
+ * Created by Ása Júlía on 29.3.2018.
+ */
+
+public class ProfileDetailsMyRequests extends android.support.v4.app.Fragment{
+    public void setArgument(Bundle argument) {
+
+    }
+}
---
+++ app/src/main/java/artyfartyparty/solo/Controller/ProfileDetailsUser.java
@@ -0,0 +1,17 @@
+package artyfartyparty.solo.Controller;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import artyfartyparty.solo.R;
+
+/**
+ * Created by Ása Júlía on 29.3.2018.
+ */
+
+public class ProfileDetailsUser extends android.support.v4.app.Fragment{
+
+
+}
